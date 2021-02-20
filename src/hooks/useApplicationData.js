0b1c0a9c79b31f5ff78bebcb72f2523e6636a3de
Jaime <jamesreuss1@gmail.com>
2021-02-19 20:28:21 -0800

    spots now updating

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -11,6 +11,19 @@ export default function useApplicationData(initial) {
   });
 
   
+function updateSpots() {
+  axios.get('/api/days')
+  .then((values) => {
+    setState(prev => ({
+      ...prev,
+      days: values.data
+    }))
+  })
+}
+
+
+
+
 function bookInterview(id, interview) {
   //console.log(id, interview);
   const appointment = {
@@ -23,8 +36,11 @@ function bookInterview(id, interview) {
   };
   const url = `/api/appointments/${id}`;
   return axios.put(url, appointment)
-    .then(() => setState({...state, appointments}));
-}
+    .then(() => setState({...state, appointments}))
+    .then(() => {
+      updateSpots()
+    });
+};
 
 function cancelInterview(id) {
   const appointment = {
@@ -37,12 +53,13 @@ function cancelInterview(id) {
   };
   const url = `/api/appointments/${id}`;
   return axios.delete(url, appointment)
-    .then(() => setState({...state, appointments}));
+    .then(() => setState({...state, appointments}))
+    .then(() => updateSpots());
 
-}
+};
 
 
-
+//{"1":{"id":1,"time":"12pm","interview":null},"2":{"id":2,"time":"1pm","interview":null},"3":{"id":3,"time":"2pm","interview":{"student":"Jaime Reuss","interviewer":5}},"4":{"id":4,"time":"3pm","interview":{"student":"Archie Cohen","interviewer":9}},"5":{"id":5,"time":"4pm","interview":null},"6":{"id":6,"time":"12pm","interview":{"student":"Chad Takahashi","interviewer":9}},"7":{"id":7,"time":"1pm","interview":null},"8":{"id":8,"time":"2pm","interview":null},"9":{"id":9,"time":"3pm","interview":{"student":"Jamal Jordan","interviewer":6}},"10":{"id":10,"time":"4pm","interview":{"student":"Leopold Silvers","interviewer":6}},"11":{"id":11,"time":"12pm","interview":{"student":"Liam Martinez","interviewer":5}},"12":{"id":12,"time":"1pm","interview":{"student":"Lydia Miller-Jones","interviewer":5}},"13":{"id":13,"time":"2pm","interview":{"student":"Maria Boucher","interviewer":3}},"14":{"id":14,"time":"3pm","interview":null},"15":{"id":15,"time":"4pm","interview":null},"16":{"id":16,"time":"12pm","interview":null},"17":{"id":17,"time":"1pm","interview":{"student":"Michael Chan-Montoya","interviewer":3}},"18":{"id":18,"time":"2pm","interview":{"student":"Allegra Scrugham","interviewer":5}},"19":{"id":19,"time":"3pm","interview":null},"20":{"id":20,"time":"4pm","interview":{"student":"Richard Wong","interviewer":9}},"21":{"id":21,"time":"12pm","interview":null},"22":{"id":22,"time":"1pm","interview":{"student":"Yuko Smith","interviewer":3}},"23":{"id":23,"time":"2pm","interview":null},"24":{"id":24,"time":"3pm","interview":null},"25":{"id":25,"time":"4pm","interview":null}}
 
 
 
